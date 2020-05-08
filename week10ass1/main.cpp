62d8ef94c934cad0b43984986731f86f52b42736
fjl18042211 <61268428+fjl18042211@users.noreply.github.com>
2020-05-08 14:49:36 +0800

    week10ass1

---
+++ week10ass1/main.cpp
@@ -0,0 +1,81 @@
+#include<opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+std::vector<Point>  mousePoints;
+Point points;
+
+void on_mouse(int EVENT, int x, int y, int flags, void* userdata)
+{
+
+	Mat hh;
+	hh = *(Mat*)userdata;
+	Point p(x, y);
+	switch (EVENT)
+	{
+	case EVENT_LBUTTONDOWN:
+	{
+		points.x = x;
+		points.y = y;
+		mousePoints.push_back(points);
+		circle(hh, points, 4, cvScalar(255, 255, 255), -1);
+		imshow("mouseCallback", hh);
+	}
+	break;
+	}
+
+}
+int selectPolygon(cv::Mat srcMat, cv::Mat &dstMat)
+{
+
+	vector<vector<Point>> contours;
+	cv::Mat selectMat;
+
+	cv::Mat m = cv::Mat::zeros(srcMat.size(), CV_32F);
+
+	m = 1;
+
+	if (!srcMat.empty()) {
+		srcMat.copyTo(selectMat);
+		srcMat.copyTo(dstMat);
+	}
+	else {
+		std::cout << "failed to read image!:" << std::endl;
+		return -1;
+	}
+
+	namedWindow("mouseCallback");
+	imshow("mouseCallback", selectMat);
+	setMouseCallback("mouseCallback", on_mouse, &selectMat);
+	waitKey(0);
+	destroyAllWindows();
+	//¼ÆËãroi
+	contours.push_back(mousePoints);
+	if (contours[0].size() < 3) {
+		std::cout << "failed to read image!:" << std::endl;
+		return -1;
+	}
+
+	drawContours(m, contours, 0, Scalar(0), -1);
+
+	m.copyTo(dstMat);
+
+	return 0;
+}
+
+
+int main()
+{
+	cv::Mat srcMat = imread("D:\\Lena.jpg");
+	cv::Mat dstMat;
+
+	selectPolygon(srcMat, dstMat);
+
+	imshow("srcMat", srcMat);
+	imshow("select Area", dstMat);
+	waitKey(0);
+
+	return 0;
+}
